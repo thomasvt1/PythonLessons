323b6abdbc3e3c3eb2c82d98faa1a4344a8af939
Thomas van Tilburg <thomasvt@me.com>
2017-10-28 12:20:41 +0200

    Colliding is now working

--- Pyg3.py
+++ Pyg3.py
@@ -6,16 +6,18 @@ import pyglet
 # Show FPS
 fps = pyglet.clock.ClockDisplay()
 
+class Spawner:
+    def __init__(self, zombies):
+        self.zombies = zombies
+
 class Collider:
-    def __init__(self, spr1, spr2):
-        self.spr1 = spr1
-        self.spr2 = spr2
+    def is_colliding(self, prj, target):
+        tx1 = target.x
+        tx2 = target.x + target.width
+        ty1 = target.y
+        ty2 = target.y + target.height
 
-    def sprite_collide(self):
-        spr1 = self.spr1
-        spr2 = self.spr2
-
-        return (spr1.x - spr2.x) ** 2 + (spr1.y - spr2.y) ** 2 < (spr1.width / 2 + spr2.width / 2) ** 2
+        return tx1 < prj.x < tx2 and ty1 < prj.y < ty2
 
 
 class Bullet:
@@ -51,13 +53,13 @@ class Background:
 
 class Zombie:
     def __init__(self, random):
-        animation = pyglet.resource.animation('resources/policeman.gif')
+        animation = pyglet.resource.animation('resources/zombie1.gif')
         self.sprite = pyglet.sprite.Sprite(animation)
         self.sprite.scale = .3
 
         if random is True:
-            self.sprite.x = randint(50, 600)
-            self.sprite.y = randint(50, 500)
+            self.sprite.x = randint(200, 500)
+            self.sprite.y = randint(50, 300)
         # marryList.insert(len(marryList), sprite)
 
     def draw(self):
@@ -92,6 +94,7 @@ class Window(pyglet.window.Window):
         self.lastshot = 0
         self.pressed_keys = []
         self.bullets = []
+        self.zombies = []
         self.mymary = Mary(True)
         self.zombie = Zombie(True)
         self.background = Background()
@@ -100,33 +103,10 @@ class Window(pyglet.window.Window):
     # it does nothing as far as I know. Just a bit of counting up!
     def update(self, dt):
         self.lastshot += dt  # Only allow shooting every .x seconds
-
-        # Make sure that walking with an angle is the same speed. We're a retro game duhh
-        steps = 150
-        if self.pressedmovekeys() > 1:
-            steps = steps / 1.5
-        print(steps)
-        steps *= dt
-
         marry = self.mymary.getsprite()
-        if key.LEFT in self.pressed_keys:
-            marry.x -= steps
-        if key.RIGHT in self.pressed_keys:
-            marry.x += steps
-        if key.DOWN in self.pressed_keys:
-            marry.y -= steps
-        if key.UP in self.pressed_keys:
-            marry.y += steps
 
-        if marry.y < 0:
-            marry.y = 0
-        if marry.x < 0:
-            marry.x = 0
-
-        if marry.y + marry.height > self.height:
-            marry.y = self.height - marry.height
-        if marry.x + marry.width > self.width:
-            marry.x = self.width - marry.width
+        self.move_marry(marry, dt)
+        self.keep_marry_in_screen(marry)
 
         if key.SPACE in self.pressed_keys:
             if self.lastshot > .5:
@@ -137,7 +117,39 @@ class Window(pyglet.window.Window):
             if b.getx() > self.width:
                 self.bullets.remove(b)
             b.update()
+
+            if Collider.is_colliding(self, b.getsprite(), self.zombie.getsprite()):
+                print("Collide!")
+                self.bullets.remove(b)
         pass
+
+    def move_marry(self, marry, dt):
+        # Make sure that walking with an angle is the same speed. We're a retro game duhh
+        steps = 150
+        if self.pressedmovekeys() > 1:
+            steps = steps / 1.5
+        steps *= dt
+
+
+        if key.LEFT in self.pressed_keys:
+            marry.x -= steps
+        if key.RIGHT in self.pressed_keys:
+            marry.x += steps
+        if key.DOWN in self.pressed_keys:
+            marry.y -= steps
+        if key.UP in self.pressed_keys:
+            marry.y += steps
+
+    def keep_marry_in_screen(self, marry):
+        if marry.y < 0:
+            marry.y = 0
+        if marry.x < 0:
+            marry.x = 0
+
+        if marry.y + marry.height > self.height:
+            marry.y = self.height - marry.height
+        if marry.x + marry.width > self.width:
+            marry.x = self.width - marry.width
 
     # Return the amount of move keys pressed
     def pressedmovekeys(self):
@@ -152,7 +164,6 @@ class Window(pyglet.window.Window):
             keys += 1
         return keys
 
-
     def on_draw(self):
         pyglet.clock.tick()  # Make sure you tick o'l the clock!
         self.clear()
@@ -162,8 +173,44 @@ class Window(pyglet.window.Window):
         self.mymary.getsprite()
         self.zombie.draw()
 
+        #self.drawborder(self.zombie.getsprite())
+
         for b in self.bullets:
             b.draw()
+        for z in self.zombies:
+            z.draw()
+
+    def drawborder(self, spr1):
+        melon = pyglet.image.load('resources/melon.png')
+        ld = pyglet.sprite.Sprite(melon)
+        ld.scale = .05
+        ld.x = self.zombie.getsprite().x
+        ld.y = self.zombie.getsprite().y
+        ld.draw()
+
+        rd = pyglet.sprite.Sprite(melon)
+        rd.scale = .05
+        rd.x = self.zombie.getsprite().x + spr1.width
+        rd.y = self.zombie.getsprite().y
+        rd.draw()
+
+        lu = pyglet.sprite.Sprite(melon)
+        lu.scale = .05
+        lu.x = self.zombie.getsprite().x
+        lu.y = self.zombie.getsprite().y + spr1.height
+        lu.draw()
+
+        ru = pyglet.sprite.Sprite(melon)
+        ru.scale = .05
+        ru.x = self.zombie.getsprite().x + spr1.width
+        ru.y = self.zombie.getsprite().y + spr1.height
+        ru.draw()
+
+        ce = pyglet.sprite.Sprite(melon)
+        ce.scale = .05
+        ce.x = self.zombie.getsprite().x + (spr1.width / 2)
+        ce.y = self.zombie.getsprite().y + (spr1.height / 2)
+        ce.draw()
 
     def on_key_press(self, symbol, modifiers):
         if symbol in self.pressed_keys:
